99aa13338dcc71e608f42172bf237056b8b3023d
raghav-malik <malikraghav2004@gmail.com>
2024-04-16 21:15:05 +0530

    update-2

---
+++ app/src/main/java/com/example/assetracker/GoldActivity.java
@@ -0,0 +1,14 @@
+package com.example.assetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GoldActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gold);
+    }
+}
