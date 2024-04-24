a99d46a3ad23353444d7943ee9263874c3c91522
PURAV <puravmalik24@gmail.com>
2024-04-24 23:13:35 +0530

    Mera pehela commit

--- app/src/main/java/com/example/assetracker/LoginActivity.java
+++ app/src/main/java/com/example/assetracker/LoginActivity.java
@@ -70,6 +70,7 @@ class ButtonHandler implements View.OnClickListener
 //            JsonObject json= (JsonObject) api.callGenericApi(user, "login", "get",null,null, null);
             JsonElement obj = api.callGenericApi(user, "assets", "get", null, null, null);
             System.out.println(obj);
+            System.out.println("Hello Guys, Purav this side");
 
         }
 
