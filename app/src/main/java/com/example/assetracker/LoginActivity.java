a79abd37cfed61f94e05b80c8507ae5509dae579
Manav <manavsharma2139@gmail.com>
2024-04-24 22:50:50 +0530

    test commit

--- app/src/main/java/com/example/assetracker/LoginActivity.java
+++ app/src/main/java/com/example/assetracker/LoginActivity.java
@@ -21,6 +21,7 @@ public class LoginActivity extends AppCompatActivity {
         user1.addProperty("password","123456789");
         JsonArray json= (JsonArray) api.callGenericApi(user1, "assets", "get",null,null,null);
         System.out.println(json);
+        System.out.println(1);
         //        btn.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
