cad91e558c65460bc2998e7f4a0195ba9b838924
nikhil-kumar-tomar <nikhiltomar931@gmail.com>
2024-05-04 02:49:33 +0530

    Added Buy and Sell functionality for stocks

--- app/src/main/java/com/example/assetracker/StocksActivity2.java
+++ app/src/main/java/com/example/assetracker/StocksActivity2.java
@@ -1,14 +1,95 @@
 package com.example.assetracker;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.assetracker.API.api;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 public class StocksActivity2 extends AppCompatActivity {
-
+    LinearLayout stocks_linearlayout_dynamic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stocks2);
+
+        stocks_linearlayout_dynamic = (LinearLayout) findViewById(R.id.stocks_linearlayout_dynamic);
+        stocks_linearlayout_dynamic.removeAllViews();
+        fetchDataForPortfolio();
+    }
+
+    private void fetchDataForPortfolio() {
+        SharedPreferences sharedPreferences = getSharedPreferences("LoginPrefs", Context.MODE_PRIVATE);
+
+
+        String username = sharedPreferences.getString("username", "admin").replace("\"", "");
+        String password = sharedPreferences.getString("password", "123456789").replace("\"", "");
+        JsonObject requestData = new JsonObject();
+        requestData.addProperty("username", username);
+        requestData.addProperty("password", password);
+        JsonElement response = api.callGenericApi(requestData, "portfolio_information", "get", "asset_type=equity&holding_type=stock", null, null);
+        JsonArray responseData = response.getAsJsonArray();
+
+        if (responseData != null)
+        {
+            LinearLayout.LayoutParams textParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+            textParams.weight = 0.3f;
+            textParams.gravity = Gravity.END;
+
+            for (JsonElement json_element: responseData) {
+                JsonObject json_object = json_element.getAsJsonObject();
+                LinearLayout rowLayout = new LinearLayout(this);
+                rowLayout.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+                rowLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+
+                Button button = new Button(this);
+                LayoutParams buttonParams = new LayoutParams(0, LayoutParams.WRAP_CONTENT, 1f);
+                button.setLayoutParams(buttonParams);
+                button.setText(json_object.get("show_name").toString().replace("\"",""));
+                button.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+
+
+                button.setTextColor(getResources().getColor(R.color.black));
+                button.setBackgroundResource(R.drawable.btnforpurposes);
+//                button.setPadding(0, 8, 0, 0);
+
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(StocksActivity2.this, StocksProcessActivity.class);
+                        intent.putExtra("id", json_object.get("id").toString());
+                        startActivity(intent);
+                    }
+                });
+
+                rowLayout.addView(button);
+
+                // We can add some text below as well Currently I am not doing it for quickness
+//                TextView textView = new TextView(this);
+//                textView.setLayoutParams(textParams);
+//                textView.setText("Text " + (i + 1));
+//                rowLayout.addView(textView);
+
+
+                stocks_linearlayout_dynamic.addView(rowLayout);
+            }
+
+        }
+        else{
+            Toast.makeText(this, "Response data is empty", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/assetracker/StocksProcessActivity.java
+++ app/src/main/java/com/example/assetracker/StocksProcessActivity.java
@@ -1,14 +1,178 @@
 package com.example.assetracker;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.assetracker.API.api;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
-public class StocksProcessActivity extends AppCompatActivity {
+import java.util.Locale;
 
+
+public class StocksProcessActivity extends AppCompatActivity  {
+
+    String id = null;
+    TextView stockbtnactivities, quantitytextview, salespricetextview;
+    Button buy, sell;
+    EditText operation_quantity; // Is temporary
+    ButtonHandlerStocksProcess btnhandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stocks_process);
+
+        stockbtnactivities = (TextView) findViewById(R.id.stockbtnactivities);
+        quantitytextview = (TextView) findViewById(R.id.quantitytextview);
+        salespricetextview = (TextView) findViewById(R.id.salespricetextview);
+
+
+        buy = (Button) findViewById(R.id.buystocksbtn);
+        sell = (Button) findViewById(R.id.sellstocksbtn);
+        operation_quantity = (EditText) findViewById(R.id.quantity_operation);
+
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra("id")) {
+            id = intent.getStringExtra("id");
+        }
+//        btnhandler = new ButtonHandlerStocksProcess(this, buy, sell, operation_quantity, id);
+        fetchDataForPortfolio();
     }
+    private void fetchDataForPortfolio() {
+        SharedPreferences sharedPreferences = getSharedPreferences("LoginPrefs", Context.MODE_PRIVATE);
+
+
+        String username = sharedPreferences.getString("username", "admin").replace("\"", "");
+        String password = sharedPreferences.getString("password", "123456789").replace("\"", "");
+        JsonObject requestData = new JsonObject();
+        requestData.addProperty("username", username);
+        requestData.addProperty("password", password);
+        JsonElement response = api.callGenericApi(requestData, "asset", "get", null, id, null);
+        JsonObject responseData = response.getAsJsonObject();
+
+        if (responseData != null)
+        {
+            stockbtnactivities.setText(responseData.get("show_name").toString().replace("\"", ""));
+            Double current_price = Double.parseDouble(responseData.get("current_price").toString().replace("\"", ""));
+            String string_current_price = String.format(Locale.US, "%.2f", current_price);
+            salespricetextview.setText(string_current_price);
+
+            JsonObject user_information = responseData.get("user_information").getAsJsonObject();
+
+            // set Top Header information invested, returns and more everything is provided in user_information
+            quantitytextview.setText(user_information.get("quantity").toString());
+            btnhandler = new ButtonHandlerStocksProcess(this, buy, sell, operation_quantity, id, requestData, this);
+
+            buy.setOnClickListener(btnhandler);
+            sell.setOnClickListener(btnhandler);
+        }
+        else{
+            Toast.makeText(this, "Response data is empty", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+
+
+}
+
+class ButtonHandlerStocksProcess implements View.OnClickListener
+{
+
+    Context context;
+    Button buy, sell;
+    EditText quantity_operation;
+    String id;
+    JsonObject requestData;
+    Intent intent;
+    AppCompatActivity app_combat;
+    ButtonHandlerStocksProcess(Context context, Button buy, Button sell)
+    {
+        this.context = context;
+        this.buy = buy;
+        this.sell = sell;
+    }
+    ButtonHandlerStocksProcess(Context context, Button buy,
+                               Button sell, EditText quantity_operation,
+                               String id, JsonObject requestData, AppCompatActivity app_combat)
+    {
+        this.context = context;
+        this.buy = buy;
+        this.sell = sell;
+        this.quantity_operation = quantity_operation;
+        this.id = id;
+        this.requestData = requestData;
+        this.app_combat = app_combat;
+
+    }
+    @Override
+    public void onClick(View v) {
+        String string_quantity = quantity_operation.getText().toString();
+
+        try {
+            int int_quantity = Integer.parseInt(string_quantity);
+            if (int_quantity <= 0)
+            {
+                throw new ArithmeticException();
+            }
+            JsonObject json_obj = new JsonObject();
+            json_obj.addProperty("asset", id);
+            json_obj.addProperty("quantity", int_quantity);
+            JsonElement response = null;
+            if (v.getId() == buy.getId()) {
+
+                Toast.makeText(context, "Buy Action Registered", Toast.LENGTH_SHORT).show();
+                json_obj.addProperty("action", "buy");
+                response = api.callGenericApi(requestData,
+                        "asset_action",
+                        "post",
+                        null,
+                        null,
+                        json_obj.toString());
+
+            } else if (v.getId() == sell.getId()) {
+                Toast.makeText(context, "Sell Action Registered", Toast.LENGTH_SHORT).show();
+                json_obj.addProperty("action", "sell");
+                response = api.callGenericApi(requestData,
+                        "asset_action",
+                        "post",
+                        null,
+                        null,
+                        json_obj.toString());
+            }
+
+            if (response != null) {
+                JsonObject final_responseData = response.getAsJsonObject();
+                app_combat.recreate();
+
+            }
+            else {
+                Toast.makeText(context, "Quantity not available right now.", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+        catch (ArithmeticException e)
+        {
+            Toast.makeText(context, "Quantity must be bigger than 0", Toast.LENGTH_SHORT).show();
+        }
+        catch (RuntimeException e)
+        {
+            Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(context, "Quantity must be integer", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
