26ce8e86d3154fd1e27b8978d78c407914f84a27
raghav-malik <malikraghav2004@gmail.com>
2024-05-03 21:36:55 +0530

    added stocks process page

---
+++ app/src/main/java/com/example/assetracker/StocksProcessActivity.java
@@ -0,0 +1,14 @@
+package com.example.assetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StocksProcessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stocks_process);
+    }
+}
